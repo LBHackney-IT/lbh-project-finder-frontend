bd717bcf70c6835fc9c5dcc7287d3090064bb88f
Miles-Alford <miles.alford@hackney.gov.uk>
2021-06-16 21:03:32 +0100

    Setup unit tests infrastructure and add header tests
    This commit installs all the necessary libraries such as react testing library and jest-dom to allow unit tests to be
    created for components. The configuration files such as SetupTests and jest.config sort out the infrastructure needed to get the tests running.
    Finally tests for the header has been added.

---
+++ components/Header/Header.spec.tsx
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/react';
+
+import Header from './Header';
+
+let mockedUseRouter = { pathname: 'pathname' };
+
+jest.mock('next/router', () => ({
+    asPath: 'path',
+    useRouter: () => mockedUseRouter,
+}));
+
+jest.mock('./Logo', () => () => 'MockedLogo');
+
+describe('Header component', () => {
+    const props = {
+        serviceName: 'test'
+    };
+
+    it('should render the service name', () => {
+        const { getByText } = render(<Header {...props} />);
+        expect(getByText('test')).toBeInTheDocument();
+    })
+
+    it('should render heading links', () => {
+
+        const { getByText } = render(<Header {...props} />);
+
+        expect(getByText('All Projects')).toBeInTheDocument();
+        expect(getByText('My Projects')).toBeInTheDocument();
+        expect(getByText('Sign Out')).toBeInTheDocument();
+    })
+
+});
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import Logo from './Logo'
 
+const loggedInNavLinks = [
+    {
+        name: 'All Projects',
+        path: '/'
+    },
+    {
+        name: 'My Projects',
+        path: '/my-projects'
+    },
+    {
+        name: 'Sign Out',
+        path: '/logout'
+    }
+]
+
 const Header = ({ serviceName }: { serviceName: string }): React.ReactElement => {
+    const [navLinks, setNavLinks] = useState<typeof loggedInNavLinks>();
+    const { pathname } = useRouter();
+    useEffect(() => {
+        setNavLinks(loggedInNavLinks)
+    }, [pathname])
     return (
         <header className="lbh-header ">
             <div className="lbh-header__main">
@@ -14,6 +36,18 @@ const Header = ({ serviceName }: { serviceName: string }): React.ReactElement =>
                             <span className="lbh-header__service-name">{serviceName}</span>
                         </a>
                     </div>
+
+                    <nav className="lbh-header__links">
+                        {navLinks && (
+                            <>
+                                {navLinks.map(({ name, path }) => (
+                                    <Link href={path} key={path}>
+                                        <a className="govuk-header__link">{name}</a>
+                                    </Link>
+                                ))}
+                            </>
+                        )}
+                    </nav>
                 </div>
             </div>
         </header>
