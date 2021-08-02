d54991a65bdd9e9cdf83030ac2a17369e4d1b5a3
Miles-Alford <miles.alford@hackney.gov.uk>
2021-08-02 17:34:02 +0100

    Add prettier and eslint

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-        '\\.(css|sass|scss)$': 'identity-obj-proxy',
-    },
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|sass|scss)$": "identity-obj-proxy",
+  },
 };
