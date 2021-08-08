11624bcf4d7780cd644d4485c0e985db641a0696
Miles-Alford <miles.alford@hackney.gov.uk>
2021-08-08 21:33:26 +0100

    Add API for get,delete & post for links & members

---
+++ api/projectLinks.ts
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { ProjectLink } from "../types";
+
+export const getLinksByProject = async (projectId: number): Promise<ProjectLink[] | []> => {
+    // const { data } = await axios.get(`/projects/${projectId}/links`, {
+    //     headers,
+    // })
+
+    // return data
+
+    return new Promise((resolve) => {
+        setTimeout(
+            () =>
+                resolve([{
+                    id: 1,
+                    project_id: 2,
+                    type: "GitHub",
+                    link: "https://github.com/",
+                }]),
+            3000
+        );
+    })
+
+}
+
+export const addProjectLink = async (project_id: number, formData: Record<string, string | number>): Promise<any> => {
+    // const { data } = await axios.post(`/projects/${project_id}/team`, formData, {
+    //     headers,
+    // })
+
+    // return data
+
+    return new Promise((resolve) => {
+        setTimeout(
+            () => resolve(console.log(`Form data is ${JSON.stringify(formData)}`)),
+            1000
+        );
+    });
+}
+
+export const removeProjectLink = async (
+    link_id: number
+): Promise<void> => {
+    // await axios.delete(
+    //   `${ENDPOINT_API}/links/${link_id}`,
+    //   {
+    //     headers,
+    //   }
+    // );
+};
---
+++ utils/projectLinks.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from "axios";
+import useSWR, { SWRResponse } from "swr";
+import { ProjectLink } from "../types";
+
+export const useProjectLinks = (
+    projectId: number
+): SWRResponse<ProjectLink[], AxiosError> => useSWR(`/api/projects/${projectId}/links`);
+
+interface addNewProjectLinkData {
+    project_id: number;
+    type: string;
+    link: string;
+}
+
+export const addProjectLink = async (
+    formData: addNewProjectLinkData
+): Promise<Record<string, string | number>> => {
+    const { data } = await axios.post(`/api/projects/${formData.project_id}/links`, formData);
+    return data;
+};
+
+export const removeProjectLink = async (
+    project_id: number,
+    link_id: number
+): Promise<Record<string, string | number>> => {
+    const { data } = await axios.delete(`/api/projects/${project_id}/links/remove/${link_id}`);
+
+    return data;
+};
