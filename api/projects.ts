8e77c3bdea03613acf99c096a4303953a49edc6c
Miles-Alford <miles.alford@hackney.gov.uk>
2021-08-22 15:27:22 +0100

    Add Next endpoint for getting & updating a project

--- api/projects.ts
+++ api/projects.ts
@@ -1,21 +1,22 @@
 import axios from "axios";
-import { Project } from "../types";
+import { Project, ProjectSearchResults, SearchFormData } from "../types";
+
+const ENDPOINT_API = process.env.ENDPOINT_API;
+
+export const getProjects = async (
+  params: any
+): Promise<ProjectSearchResults> => {
+  const { data } = await axios.get(`${ENDPOINT_API}/projects`, {
+    params: params,
+  });
+
+  return data;
+};
 
 export const getProject = async (projectId: number): Promise<Project> => {
-  return new Promise((resolve) => {
-    setTimeout(
-      () =>
-        resolve({
-          project_id: projectId,
-          project_name: "A new project",
-          description: "a long description",
-          size: "Large",
-          stage: "Discovery",
-          type: "Tech",
-        }),
-      3000
-    );
-  });
+  const { data } = await axios.get(`${ENDPOINT_API}/projects/${projectId}`);
+
+  return data;
 };
 
 export const addProject = async (formData: any): Promise<any> => {
@@ -25,4 +26,8 @@ export const addProject = async (formData: any): Promise<any> => {
       1000
     );
   });
+};
+
+export const updateProject = async (formData: any): Promise<void> => {
+  await axios.patch(`${ENDPOINT_API}/projects`, formData);
 };
--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -1,13 +1,28 @@
 import type { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 
 import { StatusCodes } from "http-status-codes";
-import { addProject } from "../../../api/projects";
+import { addProject, getProjects } from "../../../api/projects";
+import { SearchFormData } from "../../../types";
 
 const endpoint: NextApiHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   switch (req.method) {
+    case "GET":
+      try {
+        console.log("Query is: " + JSON.stringify(req.query));
+        const data = await getProjects(req.query);
+        res.status(StatusCodes.OK).json(data);
+      } catch (error) {
+        console.error("Project get error:", error?.response?.data);
+        console.error("Project get request:", req);
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ message: "Unable to get project" });
+      }
+      break;
+
     case "POST":
       try {
         const data = await addProject(req.body);
