eaebc92974c1d85e391df753ded7e9be7de5dc36
Miles-Alford <miles.alford@hackney.gov.uk>
2021-09-07 16:20:09 +0100

    Add header to my project endpoint

--- api/my-projects.ts
+++ api/my-projects.ts
@@ -3,9 +3,14 @@ import axios from "axios";
 import { ProjectMember } from "../types";
 
 const ENDPOINT_API = process.env.ENDPOINT_API;
+const API_KEY = process.env.API_KEY;
+
+const keyHeader = {
+    'x-api-key': API_KEY
+}
 
 export const getProjectsByUser = async (user_id: number): Promise<ProjectMember[] | []> => {
-    const { data } = await axios.get(`${ENDPOINT_API}/user/${user_id}/projects`);
+    const { data } = await axios.get(`${ENDPOINT_API}/user/${user_id}/projects`, { headers: keyHeader });
     console.log(data);
     return data;
 
